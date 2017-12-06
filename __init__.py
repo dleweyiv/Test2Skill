111124f36eb4ab171191193382f43523204b9b87
David <32504470+dleweyiv@users.noreply.github.com>
2017-12-06 02:45:06 -0500

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -43,14 +43,10 @@ class Test2Skill(MycroftSkill):
 				GPIO.set("GPIO2","On")
 				GPIO.set("GPIO3","Off")
 				#GPIO.set("GPIO4","Off")
-				#try:
-			#		mycroft.audio.wait_while_speaking()
-                #except:
-                #	time.sleep(5)
-                #	GPIO.set("GPIO1","Off")
-				#	GPIO.set("GPIO2","Off")
-				#	GPIO.set("GPIO3","Off")
-				#	GPIO.set("GPIO4","On")
+				try:
+					mycroft.audio.wait_while_speaking()
+                except:
+                	time.sleep(5)
 				#	pass
 				#GPIO.set("GPIO1","Off")
 				#GPIO.set("GPIO2","Off")
